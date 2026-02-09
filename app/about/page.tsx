f870cbb8c94ce0916182edf5c9028553e7de7937
crusthack <crusthack@gmail.com>
2026-02-09 13:15:53 +0900

    20260209

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,8 @@
 import BlogPost from "@/components/BlogPost";
 import type { Metadata } from "next";
 import { getPostData } from "@/lib/posts";
+import LeftSidebar from '@/components/LeftSidebar';
+import RightSidebar from '@/components/RightSidebar';
  
 export async function generateMetadata(): Promise<Metadata> {
     const post = getPostData("", 'about')
@@ -18,14 +20,24 @@ export default async function AboutPage() {
     }
  
     return (
-        <main className="grid grid-cols-[1fr_1000px_1fr] gap-8 w-full">
+        <div className="grid grid-cols-[1fr_1000px_1fr] gap-8 w-full">
+ 
+            {/* 왼쪽 사이드 컨텐츠 */}
             <div className="flex justify-end">
+              <LeftSidebar />
             </div>
+ 
+            {/* 가운데 컨텐츠 */}
             <div className="w-full mx-auto">
                 <BlogPost post={postData}/>
             </div>
+ 
+            {/* 오른쪽 사이드 컨텐츠 */}
             <div className="flex justify-start">
+              <RightSidebar />
             </div>
-        </main>
+ 
+        </div>
+ 
     );
 }
